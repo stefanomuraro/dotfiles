c42a098208421dc330a913b00d9c04ce113e40d5
Stefano Muraro <66977688+stefanomuraro@users.noreply.github.com>
2025-05-08 09:53:38 -0300

    add cleanrepo function

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -1,3 +1,14 @@
 killport() {
     sudo lsof -t -i:"$1" | xargs sudo kill -9
 }
+
+cleanrepo() {
+  gfa && gbg &&
+
+  print -n "Do you want to delete all gone branches? (y/n): "
+  read choice
+
+  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    gbgD
+  fi
+}
