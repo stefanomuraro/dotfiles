376fa23efdd816dede8832920283b0821111b281
Stefano Muraro <66977688+stefanomuraro@users.noreply.github.com>
2026-03-14 19:35:55 -0300

    set up yazi

--- .config/zsh/custom/functions.zsh
+++ .config/zsh/custom/functions.zsh
@@ -117,3 +117,12 @@ gh_pr() {
     fi
 }
 
+# Shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
